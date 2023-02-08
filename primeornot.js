2da4efa3dfa7567e305811fbf0cf2d0159d74970
Aaditya Kashyap <123922624+AadityaKashyap06@users.noreply.github.com>
2023-02-08 15:30:29 +0530

    made changes
    added comments

--- primeornot.js
+++ primeornot.js
@@ -2,7 +2,7 @@ let num=13;
 let count=0;
 for(let i=1;i<=num;i++){
 if(num%i==0){
-count++;
+count++;//it shouls be 2 in case of prime
 }
 }
-console.log(count==2?"prime":"not");
+console.log(count==2?"prime":"not");//prime number check
